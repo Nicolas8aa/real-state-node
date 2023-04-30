e9e24c6ca72535c3f89fe92f8d27ec5833a2e652
Nicolas C <nicolascuellar2305@gmail.com>
2023-04-30 17:20:25 -0500

    Created authentication forms routes, controllers and views

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,9 +1,12 @@
 const loginForm = (req, res) => {
-  res.render("auth/login", { user: "json" });
+  res.render("auth/login", { title: "Login" });
 };
 
 const registerForm = (req, res) => {
-  res.render("auth/register", { user: "json" });
+  res.render("auth/register", { title: "Register" });
+};
+const forgetPassword = (req, res) => {
+  res.render("auth/forget-password", { title: "Recover your access" });
 };
 
-export { loginForm, registerForm };
+export { loginForm, registerForm, forgetPassword };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { loginForm, registerForm } from "../controllers/user.controller.js";
+import {
+  forgetPassword,
+  loginForm,
+  registerForm,
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/login", loginForm);
 
 router.get("/register", registerForm);
+
+router.get("/forget-password", forgetPassword);
 
 export default router;
